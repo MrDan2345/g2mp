51a838afeab851bbb00266c7fd18330634d5dbf7
Dan <dan.soft.studio@gmail.com>
2015-07-21 08:47:52 +0700

    added layer editing for the scene2d poly editor texture layers. modified test project. bug fixes.

--- tools/Toolkit/TestProject/source/GameUnit.pas
+++ tools/Toolkit/TestProject/source/GameUnit.pas
@@ -8,36 +8,19 @@ uses
   G2Math,
   G2Utils,
   G2DataManager,
+  G2Scene2D,
   box2d,
   Types,
   SysUtils,
   Classes;
 
 type
-  t_draw = class (tb2_draw)
-  public
-    procedure draw_polygon(const vertices: pb2_vec2; const vertex_count: tb2_int32; const color: tb2_color); override;
-    procedure draw_solid_polygon(const vertices: pb2_vec2; const vertex_count: tb2_int32; const color: tb2_color); override;
-    procedure draw_circle(const center: tb2_vec2; const radius: tb2_float32; const color: tb2_color); override;
-    procedure draw_solid_circle(const center: tb2_vec2; const radius: tb2_float32; const axis: tb2_vec2; const color: tb2_color); override;
-    procedure draw_segment(const p0, p1: tb2_vec2; const color: tb2_color); override;
-    procedure draw_transform(const xf: tb2_transform); override;
-  end;
-
   TGame = class
-  protected
-    var font: TG2Font;
-    var draw: t_draw;
-    var gravity, position: tb2_vec2;
-    var world: tb2_world;
-    var ground_body_def, body_def: tb2_body_def;
-    var ground_body, body: pb2_body;
-    var ground_box, box: tb2_polygon_shape;
-    var fixture_def: tb2_fixture_def;
-    var time_step, angle: tb2_float32;
-    var boxes: array[0..399] of pb2_body;
-    var velocity_iterations, position_iterations: tb2_int32;
   public
+    Font: TG2Font;
+    Scene: TG2Scene2D;
+    Display: TG2Display2D;
+    PlayerEntity: TG2Scene2DEntity;
     constructor Create;
     destructor Destroy; override;
     procedure Initialize;
@@ -54,69 +37,8 @@ type
 
 var
   Game: TGame;
-  disp: TG2Display2D;
 
 implementation
-
-procedure t_draw.draw_polygon(const vertices: pb2_vec2; const vertex_count: tb2_int32; const color: tb2_color);
-  var i, i1: integer;
-  var c: TG2Color;
-  var v: pb2_vec2_arr absolute vertices;
-begin
-  c := G2Color(round(color.r * $ff), round(color.g * $ff), round(color.b * $ff), round(color.a * $ff));
-  for i := 0 to vertex_count - 1 do
-  begin
-    i1 := (i + 1) mod vertex_count;
-    disp.PrimLine(v^[i].x, v^[i].y, v^[i1].x, v^[i1].y, c);
-  end;
-end;
-
-procedure t_draw.draw_solid_polygon(const vertices: pb2_vec2; const vertex_count: tb2_int32; const color: tb2_color);
-  var i, i1, i2: integer;
-  var c: TG2Color;
-  var v: pb2_vec2_arr absolute vertices;
-begin
-  c := G2Color(round(color.r * $ff), round(color.g * $ff), round(color.b * $ff), round(color.a * $80));
-  for i := 0 to vertex_count - 2 do
-  begin
-    i1 := (i + 1) mod vertex_count;
-    i2 := (i + 2) mod vertex_count;
-    disp.PrimTriCol(
-      v^[0].x, v^[0].y,
-      v^[i1].x, v^[i1].y,
-      v^[i2].x, v^[i2].y,
-      c, c, c
-    );
-  end;
-  draw_polygon(vertices, vertex_count, color);
-end;
-
-procedure t_draw.draw_circle(const center: tb2_vec2; const radius: tb2_float32; const color: tb2_color);
-  var c: TG2Color;
-begin
-  c := G2Color(round(color.r * $ff), round(color.g * $ff), round(color.b * $ff), round(color.a * $ff));
-  disp.PrimCircleHollow(center.x, center.y, radius, c);
-end;
-
-procedure t_draw.draw_solid_circle(const center: tb2_vec2; const radius: tb2_float32; const axis: tb2_vec2; const color: tb2_color);
-  var c: TG2Color;
-begin
-  c := G2Color(round(color.r * $ff), round(color.g * $ff), round(color.b * $ff), round(color.a * $ff));
-  disp.PrimCircleCol(center.x, center.y, radius, c, c);
-  disp.PrimLine(center.x, center.y, center.x + axis.x * radius, center.y + axis.y * radius, $ff000000);
-end;
-
-procedure t_draw.draw_segment(const p0, p1: tb2_vec2; const color: tb2_color);
-  var c: TG2Color;
-begin
-  c := G2Color(round(color.r * $ff), round(color.g * $ff), round(color.b * $ff), round(color.a * $ff));
-  disp.PrimLine(p0.x, p0.y, p1.x, p1.y, c);
-end;
-
-procedure t_draw.draw_transform(const xf: tb2_transform);
-begin
-
-end;
 
 //TGame BEGIN
 constructor TGame.Create;
@@ -157,102 +79,39 @@ procedure TGame.Initialize;
 begin
   Font := TG2Font.Create;
   Font.Make(32);
-  gravity.set_value(0, 10);
-  world.create(gravity);
-  //world.set_allow_sleeping(false);
-  world.set_continuous_physics(true);
-  ground_body_def := b2_body_def;
-  ground_body_def.position.set_value(0, 10);
-  ground_body := world.create_body(ground_body_def);
-  ground_box.create;
-  ground_box.set_as_box(100, 10);
-  ground_body^.create_fixture(@ground_box, 0);
-  body_def := b2_body_def;
-  body_def.body_type := b2_dynamic_body;
-  box.create;
-  box.set_as_box(1, 1);
-  fixture_def := b2_fixture_def;
-  fixture_def.shape := @box;
-  fixture_def.density := 1;
-  fixture_def.friction := 0.3;
-  for i := 0 to High(boxes) do
-  begin
-    body_def.position := b2_vec2(Random(301) * 0.1 - 15, -Random(301) * 0.1 - 5);
-    body_def.angle := G2Random2Pi;
-    boxes[i] := world.create_body(body_def);
-    boxes[i]^.create_fixture(fixture_def);
-  end;
-  time_step := 1 / g2.Params.TargetUPS;
-  velocity_iterations := 8;
-  position_iterations := 3;
-  draw := t_draw.create;
-  draw.draw_flags := draw.draw_flags + [b2_df_shape, b2_df_joint, {b2_df_aabb, b2_df_pair,} b2_df_center_of_mass];
-  world.set_debug_draw(draw);
-  disp := TG2Display2D.Create;
-  disp.Position := G2Vec2(0, 0);
-  disp.Zoom := 20;
+  Display := TG2Display2D.Create;
+  Display.Width := 10;
+  Display.Height := 10;
+  Display.Position := G2Vec2(0, 0);
+  Scene := TG2Scene2D.Create;
+  Scene.Load('../assets/scene2.g2s2d');
+  Scene.Gravity := G2Vec2(0, 9.8);
+  Scene.Simulate := True;
+  Scene.EnablePhysics;
+  PlayerEntity := Scene.FindEntityByName('Player');
 end;
 
 procedure TGame.Finalize;
 begin
-  draw.free;
-  world.destroy;
+  Scene.Free;
+  Display.Free;
   Font.Free;
 end;
 
 procedure TGame.Update;
 begin
-  world.step(time_step, velocity_iterations, position_iterations);
+  if PlayerEntity <> nil then
+  Display.Position := PlayerEntity.Transform.p;
 end;
 
 procedure TGame.Render;
-  var c: pb2_contact;
-  var b: pb2_body;
-  var vl: tb2_vec2;
-  var va: tb2_float32;
-  var tp: TG2Vec2;
-  var wm: tb2_world_manifold;
 begin
-  world.draw_debug_data;
-  //c := world.get_contact_list;
-  //while c <> nil do
-  //begin
-  //  c^.get_world_manifold(wm);
-  //  disp.PrimCircleCol(
-  //    wm.points[0].x, wm.points[0].y,
-  //    0.1, $ffff0000, $ffff0000
-  //  );
-  //  c := c^.get_next;
-  //end;
-  //b := world.get_body_list;
-  //while b <> nil do
-  //begin
-  //  vl := b^.get_linear_velocity;
-  //  va := b^.get_angular_velocity;
-  //  tp := disp.CoordToScreen(G2Vec2(b^.get_position.x, b^.get_position.y));
-  //  //Font.Print(
-  //  //  tp.x, tp.y,
-  //  //  0.5, 0.5,
-  //  //  '(' + FloatToStr(vl.x) + '; ' + FloatToStr(vl.y) + '); ' + FloatToStr(va)
-  //  //);
-  //  b := b^.get_next;
-  //end;
-  g2.Window.Caption := inttostr(world.get_stack_allocator^.get_max_allocation);
+  Scene.Render(Display);
 end;
 
 procedure TGame.KeyDown(const Key: Integer);
-  var i: integer;
 begin
-  if Key = G2K_Space then
-  begin
-    for i := 0 to High(boxes) do
-    begin
-      boxes[i]^.set_transform(b2_vec2(Random(301) * 0.1 - 15, -Random(301) * 0.1 - 5), G2Random2Pi);
-      boxes[i]^.set_awake(true);
-    end;
-  end;
-  if Key = G2K_S then
-  world.step(time_step, velocity_iterations, position_iterations);
+
 end;
 
 procedure TGame.KeyUp(const Key: Integer);
@@ -272,10 +131,8 @@ end;
 
 procedure TGame.Scroll(const y: Integer);
 begin
-  if y > 0 then
-  disp.Zoom := disp.Zoom / 1.1
-  else
-  disp.Zoom := disp.Zoom * 1.1;
+  if y > 0 then Display.Zoom := Display.Zoom * 1.1
+  else Display.Zoom := Display.Zoom / 1.1;
 end;
 
 procedure TGame.Print(const c: AnsiChar);
