3908311db882950489ba8e858f7aaf36cf1b9480
Dan <dan.soft.studio@gmail.com>
2015-08-24 18:44:30 +0700

    modified the test project

--- tests/TestProject/source/GameUnit.pas
+++ tests/TestProject/source/GameUnit.pas
@@ -61,7 +61,7 @@ begin
   g2.Params.MaxFPS := 100;
   g2.Params.Width := 768;
   g2.Params.Height := 768;
-  g2.Params.ScreenMode := smWindow;
+  g2.Params.ScreenMode := smMaximized;
 end;
 
 destructor TGame.Destroy;
@@ -88,14 +88,15 @@ begin
   Display.Width := 10;
   Display.Height := 10;
   Display.Position := G2Vec2(0, 0);
+  Display.Zoom := 0.5;
   Scene := TG2Scene2D.Create;
-  Scene.Load('../assets/scene1.g2s2d');
+  Scene.Load('../assets/scene.g2s2d');
   Scene.Gravity := G2Vec2(0, 9.8);
   Scene.Simulate := True;
   Scene.EnablePhysics;
-  PlayerEntity := Scene.FindEntityByName('Car');
-  Wheel0 := Scene.FindEntityByName('Wheel0');
-  Wheel1 := Scene.FindEntityByName('Wheel1');
+  //PlayerEntity := Scene.FindEntityByName('Car');
+  //Wheel0 := Scene.FindEntityByName('Wheel0');
+  //Wheel1 := Scene.FindEntityByName('Wheel1');
   rt := TG2Texture2DRT.Create;
   rt.Make(64, 64);
 end;
@@ -111,31 +112,31 @@ end;
 procedure TGame.Update;
   var rb: TG2Scene2DComponentRigidBody;
 begin
-  if PlayerEntity <> nil then
-  Display.Position := PlayerEntity.Transform.p;
-  if g2.KeyDown[G2K_Right] then
-  begin
-    rb := TG2Scene2DComponentRigidBody(Wheel0.ComponentOfType[TG2Scene2DComponentRigidBody]);
-    rb.PhysBody^.apply_torque(EnginePower, true);
-    rb := TG2Scene2DComponentRigidBody(Wheel1.ComponentOfType[TG2Scene2DComponentRigidBody]);
-    rb.PhysBody^.apply_torque(EnginePower, true);
-  end
-  else if g2.KeyDown[G2K_Left] then
-  begin
-    rb := TG2Scene2DComponentRigidBody(Wheel0.ComponentOfType[TG2Scene2DComponentRigidBody]);
-    rb.PhysBody^.apply_torque(-EnginePower, true);
-    rb := TG2Scene2DComponentRigidBody(Wheel1.ComponentOfType[TG2Scene2DComponentRigidBody]);
-    rb.PhysBody^.apply_torque(-EnginePower, true);
-  end;
+//  if PlayerEntity <> nil then
+//  Display.Position := PlayerEntity.Transform.p;
+//  if g2.KeyDown[G2K_Right] then
+//  begin
+//    rb := TG2Scene2DComponentRigidBody(Wheel0.ComponentOfType[TG2Scene2DComponentRigidBody]);
+//    rb.PhysBody^.apply_torque(EnginePower, true);
+//    rb := TG2Scene2DComponentRigidBody(Wheel1.ComponentOfType[TG2Scene2DComponentRigidBody]);
+//    rb.PhysBody^.apply_torque(EnginePower, true);
+//  end
+//  else if g2.KeyDown[G2K_Left] then
+//  begin
+//    rb := TG2Scene2DComponentRigidBody(Wheel0.ComponentOfType[TG2Scene2DComponentRigidBody]);
+//    rb.PhysBody^.apply_torque(-EnginePower, true);
+//    rb := TG2Scene2DComponentRigidBody(Wheel1.ComponentOfType[TG2Scene2DComponentRigidBody]);
+//    rb.PhysBody^.apply_torque(-EnginePower, true);
+//  end;
 end;
 
 procedure TGame.Render;
 begin
-  g2.RenderTarget := rt;
-  Display.ViewPort := Rect(0, 0, 64, 64);
+  //g2.RenderTarget := rt;
+  //Display.ViewPort := Rect(0, 0, 64, 64);
   Scene.Render(Display);
-  g2.RenderTarget := nil;
-  g2.PicRect(0, 0, 768, 768, $ffffffff, rt, bmNormal, tfPoint);
+  //g2.RenderTarget := nil;
+  //g2.PicRect(0, 0, 768, 768, $ffffffff, rt, bmNormal, tfPoint);
 end;
 
 procedure TGame.KeyDown(const Key: Integer);
